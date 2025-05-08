b528fcab3ac4d0f35317c17c6d0015b09acec2d0
Yoonwhan <yoonwhan0@naver.com>
2025-05-08 21:13:30 +0900

    123

---
+++ start/script..js
@@ -0,0 +1,30 @@
+document
+.getElementById("loginForm")
+.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errorMsg = document.getElementById("errorMsg");
+    const container = document.getElementById("container");
+    const footer = document.getElementById("footer");
+
+    container.style.animation = "fadeOut 1s forwards";
+    footer.classList.add("footer-fade-out");
+
+    setTimeout(function () {
+        const validUsername = "admin";
+        const validPassword = "1234";
+
+        if (
+            username === validUsername &&
+            password === validPassword
+        ) {
+            window.location.href =
+                "./agentforum/agentforum.html";
+        } else {
+            errorMsg.style.display = "block";
+            container.style.animation = ""; // 애니메이션 초기화
+            footer.classList.remove("footer-fade-out");
+        }
+    }, 1000);
+});
